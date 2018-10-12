ba1b9352455eaef59832fed5f9473ad3c97391ca
RickyGomez10 <00108517@uca.edu.sv>
2018-10-12 10:33:19 -0600

    Arreglado un error de variables

--- asset/js/Script.js
+++ asset/js/Script.js
@@ -6,8 +6,8 @@ container.style.width = "200px";
 container.style.height = "200px";
 container.innerHTML = "<input type=”text” placeholder=’escriba su texto’>";
 
-container = document.getElementsByClassName("container c2");
-for(let i of container){
+var containers = document.getElementsByClassName("container c2");
+for(let i of containers){
     i.style.backgroundColor = "#009999";
     i.innerHTML = "<input type='text' placeholder='Escriba su texto'>";
 
